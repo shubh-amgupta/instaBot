b8687a395622750e815f86766ae0adfbad394282
Shubham Gupta <shubhamg.0907@gmail.com>
2017-10-20 14:15:00 +0530

    Search User

--- main.py
+++ main.py
@@ -19,7 +19,42 @@ def self_info():
         else:
             print 'User does not exist!'
     else:
-        print 'Error Occured. Try after sometime.'
+        print 'Error Occurred. Try after sometime.'
+
+
+def get_user_id(insta_username):
+    request_url = (BASE_URL + 'users/search?q=%s&access_token=%s') % (insta_username, ACCESS_TOKEN)
+    user_info = requests.get(request_url).json()
+
+    if user_info['meta']['code'] == 200:
+        if len(user_info['data']):
+            return user_info['data'][0]['id']
+        else:
+            return None
+    else:
+        print 'Error Occurred. Try after sometime.'
+        exit()
+
+
+def get_user_info(insta_username):
+    user_id = get_user_id(insta_username)
+    if user_id == None:
+        print 'User does not exist!'
+        exit()
+    request_url = (BASE_URL + 'users/%s?access_token=%s') % {user_id, ACCESS_TOKEN}
+    print 'GET request url : %s' % (request_url)
+    user_info = requests.get(request_url).json()
+
+    if user_info['meta']['code'] == 200:
+        if len(user_info['data']):
+            print 'Username: %s' % (user_info['data']['username'])
+            print 'No. of followers: %s' % (user_info['data']['counts']['followed_by'])
+            print 'No. of people you are following: %s' % (user_info['data']['counts']['follows'])
+            print 'No. of posts: %s' % (user_info['data']['counts']['media'])
+        else:
+            print 'There is no data for this user!'
+    else:
+        print 'Error Occurred. Try after sometime.'
 
 
 self_info()
